abae5a4535bd884bd5c78a11540d2bd6153b9335
olfa199 <olfa.dallel.tt@gmail.com>
2022-06-02 12:32:01 +0000

    add chain configurations

--- execution/chains/jupiter/configuration.sh
+++ execution/chains/jupiter/configuration.sh
@@ -3,3 +3,6 @@ crossdomaind query crossdomain show-local-domain --node tcp://localhost:26668
 crossdomaind query crossdomain show-local-domain-certificate --node tcp://localhost:26668
 crossdomaind query crossdomain  show-private-key  --node tcp://localhost:26668
 crossdomaind query crossdomain show-root-certificate --node tcp://localhost:26668
+
+crossdomaind tx crossdomain  create-forward-policy "broadcast" "" "" --from alice --yes --home ~/.jupiter --chain-id jupiter --node tcp://localhost:26668 --gas=auto --gas-adjustment=1.15
+crossdomaind query crossdomain show-forward-policy --node tcp://localhost:26668
--- execution/chains/neptune/configuration.sh
+++ execution/chains/neptune/configuration.sh
@@ -3,3 +3,6 @@ crossdomaind query crossdomain show-local-domain --node tcp://localhost:26670
 crossdomaind query crossdomain show-local-domain-certificate --node tcp://localhost:26670
 crossdomaind query crossdomain  show-private-key  --node tcp://localhost:26670
 crossdomaind query crossdomain show-root-certificate --node tcp://localhost:26670
+
+crossdomaind tx crossdomain  create-forward-policy "broadcast" "" "" --from alice --yes --home ~/.neptune --chain-id neptune --node tcp://localhost:26670 --gas=auto --gas-adjustment=1.15
+crossdomaind query crossdomain show-forward-policy --node tcp://localhost:26670
