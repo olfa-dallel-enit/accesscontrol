48bae1714e27fb98f973b6994dffe83fef34d825
olfa199 <olfa.dallel.tt@gmail.com>
2022-06-02 13:09:47 +0000

    add authentication

--- execution/chains.sh
+++ execution/chains.sh
@@ -1,3 +1,5 @@
+
+
 ignite chain serve -c execution/chains/earth/earth.yml --reset-once
 ignite chain serve -c execution/chains/mars/mars.yml --reset-once
 ignite chain serve -c execution/chains/venus/venus.yml --reset-once
---
+++ execution/relayerConfiguration.sh
@@ -0,0 +1,6 @@
+rm -rf ~/.ignite/relayer
+
+ignite relayer configure -a --source-rpc "http://0.0.0.0:26657" --source-faucet "http://0.0.0.0:4500" --source-port "cdaccesscontrol" --source-version "cdaccesscontrol-1" --source-gasprice "0.0000025stake" --source-prefix "cosmos" --source-gaslimit 300000 --target-rpc "http://0.0.0.0:26659" --target-faucet "http://0.0.0.0:4501" --target-port "cdaccesscontrol" --target-version "cdaccesscontrol-1" --target-gasprice "0.0000025stake" --target-prefix "cosmos" --target-gaslimit 300000 --ordered
+
+
+ignite relayer connect
