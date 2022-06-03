769fd13370adb5433b4984f8c991b78f5df97dd9
olfa199 <olfa.dallel.tt@gmail.com>
2022-06-03 07:48:49 +0000

    add cooperation

--- execution/commands.sh
+++ execution/commands.sh
@@ -10,8 +10,22 @@ crossdomaind query cdaccesscontrol list-domain-cooperation --node tcp://localhos
 
 crossdomaind tx cdaccesscontrol send-disable-cooperation  cdaccesscontrol channel-0 --from alice --yes --home ~/.earth --chain-id earth --node tcp://localhost:26657 --gas=auto --gas-adjustment=1.15
 
+crossdomaind query cdaccesscontrol list-domain-cooperation --node tcp://localhost:26657
+
 crossdomaind tx cdaccesscontrol send-enable-cooperation  cdaccesscontrol channel-0 --from alice --yes --home ~/.mars --chain-id mars --node tcp://localhost:26659 --gas=auto --gas-adjustment=1.15
 
 crossdomaind query cdaccesscontrol list-domain-cooperation --node tcp://localhost:26657
 
+crossdomaind tx cdaccesscontrol send-modify-cooperation-cost  cdaccesscontrol channel-0  300 --from alice --yes --home ~/.earth --chain-id earth --node tcp://localhost:26657 --gas=auto --gas-adjustment=1.15
+
+crossdomaind query cdaccesscontrol list-domain-cooperation --node tcp://localhost:26657
+
+crossdomaind tx cdaccesscontrol send-modify-cooperation-validity  cdaccesscontrol channel-0 "2022-01-01 00:00:00" "2026-01-01 00:00:00" --from alice --yes --home ~/.mars --chain-id mars --node tcp://localhost:26659 --gas=auto --gas-adjustment=1.15
+
+crossdomaind query cdaccesscontrol list-domain-cooperation --node tcp://localhost:26657
+
+
+
 crossdomaind query cdaccesscontrol list-cooperation-log --node tcp://localhost:26657
+
+
